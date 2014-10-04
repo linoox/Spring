c3678618743f77cfd57a17318166f52ffe97e3a1
linoox <pd2614g@gmail.com>
2014-10-03 23:00:26 -0400

    simple controller

---
+++ trippie/src/test/java/trippie/web/controller/HelloControllerTests.java
@@ -0,0 +1,17 @@
+package trippie.web.controller;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HelloControllerTests extends TestCase{
+
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+
+}
